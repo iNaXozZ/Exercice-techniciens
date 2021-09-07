6023a63001ac2928fa4b1428a1775e0e9945e161
iNaXozZ <romain.tacon.coc@gmail.com>
2021-09-07 08:20:05 +0200

    Création des classes et des répertoire Modèle et Rendu

---
+++ Exercice techniciens/Modèle/Technicien.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_techniciens.Modèle
+{
+    public class Technicien
+    {
+    }
+}
